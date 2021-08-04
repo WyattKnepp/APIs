a7fc8bc78bbe88eeda00a13b53d27a4f6082ff19
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-04 17:55:20 -0500

    Finished

--- APIs/ContentView.swift
+++ APIs/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	@State private var entries = [Entry]()
+	@State private var showingAlert = false
+	
+	var body: some View {
+		
+		NavigationView {
+			
+			List(entries) { entry in
+				Link(destination: URL(string: entry.link)!, label: {
+					Text(entry.title)
+				})
+			}
+			.navigationTitle("Good Boys")
+			
+		}
+		.onAppear(perform: {
+			queryAPI()
+		})
+		.alert(isPresented: $showingAlert, content: {
+			Alert(title: Text("Error"),
+			      message: Text("Data wouldn't load"),
+			      dismissButton: .default(Text("K")))
+		})
+	}
+	func queryAPI() {
+		let apiKey = "?rapidapi-key=f59e2b55ebmsh66bfe4e6e09fee8p1aa206jsn60690112c227"
+		let query = "https://google-search3.p.rapidapi.com/api/v1/search/q=dog&num=100\(apiKey)"
+		
+		if let url = URL(string: query) {
+			if let data = try? Data(contentsOf: url) {
+				let json = try! JSON(data: data)
+				let contents = json["results"].arrayValue
+				for item in contents {
+					let title = item["title"].stringValue
+					let link = item["link"].stringValue
+					let entry = Entry(title: title, link: link)
+					
+					entries.append(entry)
+				}
+				return
+			}
+		}
+		showingAlert = true
+	}
+	
 }
-
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
+}
+struct Entry: Identifiable {
+	let id = UUID()
+	let title: String
+	let link: String
 }
