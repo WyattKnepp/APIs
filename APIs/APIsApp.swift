835887edd459e4d7d525c7ab5799cd8f0a15898b
Wyatt Knepp <wyattjoeknepp@gmail.com>
2021-08-04 17:12:23 -0500

    Initial Commit

---
+++ APIs/APIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIsApp.swift
+//  APIs
+//
+//  Created by Wyatt Knepp on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct APIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
